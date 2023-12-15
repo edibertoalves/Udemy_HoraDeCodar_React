4e603a22168ef4377f15c1443effc500e5748507
edibertoalves <ediberto@gmail.com>
2023-12-14 21:26:29 -0300

    capítulo 4 de abordagem do CSS no React

---
+++ 4_CSS_REACT/css/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './components/MyComponent' 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  var n = 9;
+
+  const [name] = useState('Ediberto')
+
+  const redTitle = false;
+
+  return (
+    <>
+      {/* CSS Global */}
+      <h1>React com CSS</h1>
+
+      {/* CSS de componente */}
+      <MyComponent />
+      <p>Este paragrafo é do App.jsx</p>
+
+      {/* css inline */}
+      <p style={{ color: 'red', padding: '25px', borderTop: '1px solid red' }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={{ color: 'red', padding: '25px', borderTop: '1px solid red' }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+
+      {/* css inlne dinamico */}
+      <h2 style={n < 10 ? ({ color: 'purple' }) : ({ color: 'pink' })}>css dinâmico</h2>
+      <h2 style={n > 10 ? ({ color: 'purple' }) : ({ color: 'pink' })}>css dinâmico</h2>
+      <h2 style={
+                  name === "Ediberto" 
+                    ? ({ color: 'green', backgroundColor: '#000' }) 
+                    : null 
+                }>
+        Teste Nome
+      </h2>
+
+      {/* classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter uma classe dinâmica
+      </h2>
+
+    </>
+  )
+}
+
+export default App
---
+++ 4_CSS_REACT/css/src/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+import './myComponent.css'
+
+const myComponent = () => {
+  return (
+    // <div>myComponent</div>
+    <div>
+        <h1>Css de componente</h1>
+        <p>Este é o paragrafo do componente</p>
+        <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  )
+}
+
+export default myComponent
